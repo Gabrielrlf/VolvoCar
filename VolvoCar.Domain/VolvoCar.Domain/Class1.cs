83117f15f71cec9ad81f6b0a00f3bb39512162e7
Gabriel Fonseca <biellimaet@gmail.com>
2021-10-22 23:09:40 -0300

    Criando estruturas do projeto

---
+++ VolvoCar.Domain/VolvoCar.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VolvoCar.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ VolvoCar.Infra/VolvoCar.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VolvoCar.Infra
+{
+    public class Class1
+    {
+    }
+}
